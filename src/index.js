8a3f90adbaf0532ff2cb0d33937f6da1b9f005a3
hieu453 <hieult0405@gmail.com>
2022-07-22 13:17:12 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
+import { engine } from 'express-handlebars';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+import morgan from 'morgan';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(morgan('combined'));
 
+app.engine('.hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 })
 
 app.listen(port, () => {
