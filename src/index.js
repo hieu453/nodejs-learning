16150ad8583c0cc03eac83b5c7419b02a27a7b6e
hieu453 <hieult0405@gmail.com>
2022-07-27 14:09:47 +0700

    install package

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
-import { engine } from 'express-handlebars';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import express from 'express';
-import morgan from 'morgan';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const route = require('./routes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(express.json());
+
 app.use(morgan('combined'));
 
-app.engine('.hbs', engine({
+app.engine('.hbs', handlebars.engine({
     extname: '.hbs'
 }));
+
 app.set('view engine', '.hbs');
+
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-})
+});
+
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const newsRouter = require('./news.js');
+const siteRouter = require('./site.js');
+
+function route(app) {
+  app.use('/news', newsRouter);
+
+  app.use('/', siteRouter);
+  
+}
+
+module.exports = route;
+
+const array = [
+    1,
+    2
+];
